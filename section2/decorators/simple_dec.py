94fe6f972b61ab1a10c7c757465ecf03ba7a52c4
midorleave <julii.bolgar@gmail.com>
2021-06-09 21:03:47 +0200

    next lesson

--- section2/decorators/simple_dec.py
+++ section2/decorators/simple_dec.py
@@ -1,19 +1,28 @@
+import functools
+
 user = {"username": "pomking", "access_level": "guest"}
 
-def get_admin_password():
-    return "1234"
 
 def make_secure(func):
-    def secure_function():
+    @functools.wraps(func)
+    def secure_function(*args, **kwargs):
         if user["access_level"] == "admin":
-            return func()
+            return func(*args, **kwargs)
         else:
             return f"No admin permissions for {user['username']}."
 
     return secure_function
 
 
-get_admin_password = make_secure(get_admin_password)
+@make_secure
+def get_password(panel):
+    if panel == "admin":
+        return "1234"
+    elif panel == "billing":
+        return "super_secure_password"
 
-print(get_admin_password())
+#get_admin_password = make_secure(get_admin_password)
+
+#print(get_password.__name__)
+print(get_password("billing"))
 
